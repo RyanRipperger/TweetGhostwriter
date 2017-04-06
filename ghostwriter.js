4db4e48d380f9cbbb365371ed33cbe15eba00748
Ryan Ripperger <rripperger@mail.csuchico.edu>
2017-04-06 12:05:56 -0700

    Finished Lab x21

--- ghostwriter.js
+++ ghostwriter.js
@@ -1,6 +1,17 @@
 function writeTweet()
 {
-  
+  var randIndex;
+  var sentence;
+  var intro = ["Have you ever noticed that", "Its disturbing how", "What I don't understand is", "Isn't it crazy that"];
+  var body = ["cats", "computers", "video games", "iphones", "school lunches", "this weather"];
+  var end = ["are really good at wasting your time", "can sometimes drive you insane"];
+  var randIndexIntro = randomUpTo(intro.length-1);
+  sentence = intro[randIndexIntro];
+  var randIndexBody = randomUpTo(body.length-1);
+  sentence = sentence + " " + body[randIndexBody];
+  var randIndexEnd = randomUpTo(end.length-1);
+  sentence = sentence + " " + end[randIndexEnd] + ".";
+  document.getElementById("tweet").innerHTML = sentence;
 }
 
 //Generates a random whole number between 0 and max (inclusive)
